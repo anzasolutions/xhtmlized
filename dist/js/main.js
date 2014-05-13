fd8fcf0001d038beb8b4997b7cbb473752205e1a
Andrzej Zalewski <anzasolutions@gmail.com>
2014-05-13 21:31:28 +0200

    Sidebar is ready

---
+++ dist/js/main.js
@@ -0,0 +1,43 @@
+/* ==========================================================================
+
+    Project: Home
+    Author: 
+    Last updated: Tue May 13 2014 21:29:23
+
+   ========================================================================== */
+
+(function($) {
+
+  'use strict';
+
+  var App = {
+
+    /**
+     * Init Function
+     */
+    init: function() {
+      // App.feature1();
+      // App.feature2();
+    },
+
+    /**
+     * Custom feature 1
+     */
+    feature1: function() {
+
+    },
+
+    /**
+     * Custom feature 2
+     */
+    feature2: function() {
+
+    }
+
+  };
+
+  $(function() {
+    App.init();
+  });
+
+})(jQuery);
